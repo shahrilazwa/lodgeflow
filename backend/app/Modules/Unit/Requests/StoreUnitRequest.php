fcde88a76d04c3a9e24773202571dfd104c33503
shahrilazwa <shahrilazwa94@gmail.com>
2026-05-18 18:07:24 +0800

    feat: validate unit nightly price on create

--- backend/app/Modules/Unit/Requests/StoreUnitRequest.php
+++ backend/app/Modules/Unit/Requests/StoreUnitRequest.php
@@ -28,6 +28,7 @@ class StoreUnitRequest extends FormRequest
             ],
             'type' => ['required', 'string', Rule::in(Unit::TYPES)],
             'description' => ['nullable', 'string', 'max:500'],
+            'price_per_night' => ['nullable', 'numeric', 'min:0', 'max:99999999.99'],
         ];
     }
 
