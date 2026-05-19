0127d830b54473afe0b8a8addc49a4de4c4da452
shahrilazwa <shahrilazwa94@gmail.com>
2026-05-19 13:33:41 +0800

    style: fix facility model spacing

--- backend/app/Modules/Facility/Models/Facility.php
+++ backend/app/Modules/Facility/Models/Facility.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Facility extends Model
 {
     public const SCOPE_PROPERTY = 'property';
+
     public const SCOPE_UNIT = 'unit';
+
     public const SCOPE_BOTH = 'both';
 
     public const SCOPES = [self::SCOPE_PROPERTY, self::SCOPE_UNIT, self::SCOPE_BOTH];
