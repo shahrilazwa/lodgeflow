d8611bc20268af8afc007e466c5f8dfd9c7f4a1a
shahrilazwa <shahrilazwa94@gmail.com>
2026-05-20 03:16:52 +0800

    feat: allow custom facility creation

--- backend/app/Modules/Facility/Controllers/FacilityController.php
+++ backend/app/Modules/Facility/Controllers/FacilityController.php
@@ -5,6 +5,8 @@ namespace App\Modules\Facility\Controllers;
 use App\Http\Controllers\Controller;
 use App\Modules\Facility\Models\Facility;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class FacilityController extends Controller
 {
@@ -15,5 +17,24 @@ class FacilityController extends Controller
             ->get();
 
         return response()->json(['data' => $facilities]);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:100', 'unique:facilities,name'],
+            'category' => ['required', 'string', 'max:100'],
+            'scope' => ['required', 'string', Rule::in(Facility::SCOPES)],
+            'icon' => ['nullable', 'string', 'max:100'],
+        ]);
+
+        $facility = Facility::create([
+            'name' => $data['name'],
+            'category' => $data['category'],
+            'scope' => $data['scope'],
+            'icon' => $data['icon'] ?? null,
+        ]);
+
+        return response()->json(['data' => $facility], 201);
     }
 }
